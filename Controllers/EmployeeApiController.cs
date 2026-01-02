2191a626e33d17c70a68c99a6364878d340096b6
ranveernaik <ranveernaik25@gmail.com>
2026-01-02 18:47:33 +0530

    Day 12 Api Controller

---
+++ Controllers/EmployeeApiController.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeManagementCore.Models;
+using EmployeeManagementCore.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeApiController : ControllerBase
+    {
+        private readonly IEmployeeServices employeeServices;
+
+        public EmployeeApiController(IEmployeeServices services)
+        {
+            employeeServices = services;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(employeeServices.GetAllEmployees());
+        }
+
+        [HttpPost]
+        public IActionResult CreateEmployee(Employee employee)
+        {
+            employeeServices.AddEmployee(employee);
+            return Created("", employee);
+        }
+    }
+}
